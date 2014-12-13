6d546aac4124ed8a719b26e5c140c7d565440035
Paul English <paul@onfrst.com>
2014-12-13 16:57:44 -0700

    Update code using 2to3 default settings

--- docs/conf.py
+++ docs/conf.py
@@ -5,20 +5,20 @@ extensions = []
 templates_path = []
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'django-trending'
-package = u'trending'
+project = 'django-trending'
+package = 'trending'
 copyright_holder = 'Eldarion'
-copyright = u'2012, %s' % copyright_holder
+copyright = '2012, %s' % copyright_holder
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 html_theme = 'default'
 htmlhelp_basename = '%sdoc' % project
 latex_documents = [
-    ('index', '%s.tex' % project, u'%s Documentation' % project,
+    ('index', '%s.tex' % project, '%s Documentation' % project,
      copyright_holder, 'manual'),
 ]
 man_pages = [
-    ('index', project, u'%s Documentation' % project,
+    ('index', project, '%s Documentation' % project,
      [copyright_holder], 1)
 ]
 
